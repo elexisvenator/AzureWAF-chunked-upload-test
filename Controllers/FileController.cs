efcdb8500fddb954cc4250a5c76ba59a660af417
Ben Edwards <ben.edwards@veritec.com.au>
2017-06-29 09:54:12 +1000

    Added comments to controller and view.

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -17,17 +17,45 @@ namespace ChunkedUploadTest.Controllers
             this.cache = cache;
         }
 
+        /// <summary>
+        /// Check to see if a chunk has already been uploaded.
+        /// </summary>
+        /// <param name="resumableIdentifier">
+        /// The unique identifier for this file.</param>
+        /// <param name="resumableChunkNumber">
+        /// The index of the current chunk.</param>
+        /// <returns>
+        /// <code>200 OK</code> if the specified chunk is already uploaded, otherwise <code>204 No content</code>.
+        /// </returns>
         [HttpGet]
         public IActionResult Get(Guid resumableIdentifier, int resumableChunkNumber)
         {
+            // Check the memory cache to see if a reference for this chunk already exists.
             return this.cache.TryGetValue($"{resumableIdentifier}-{resumableChunkNumber}", out var _)
                        ? this.StatusCode(200)
                        : this.StatusCode(204);
         }
 
+        /// <summary>
+        /// Receive the uploaded chunk file and store it.
+        /// If all chunks are received then assemble the file.
+        /// </summary>
+        /// <param name="resumableIdentifier">
+        /// The unique identifier for this file.
+        /// </param>
+        /// <param name="resumableChunkNumber">
+        /// The index of the current chunk.
+        /// </param>
+        /// <param name="chunk">
+        /// The file chunk.
+        /// </param>
+        /// <returns>
+        /// <code>200 OK </code> to indicate the the chunk was successfully uploaded.
+        /// </returns>
         [HttpPost]
         public IActionResult Post(Guid resumableIdentifier, int resumableChunkNumber, IFormFile chunk)
         {
+            // As we are not actually storing the file in this example, instead just remember that we have received this hunk by storing it's id in a memory cache.
             this.cache.Set($"{resumableIdentifier}-{resumableChunkNumber}", true);
             return this.StatusCode(200);
         }
--- README.md
+++ README.md
@@ -16,12 +16,12 @@ These steps will skip a lot of the configuration involved in setting up the appl
 
 1. Create an Azure Application Gateway in a new resource group. The gateway needs to have WAF enabled and in protection mode.
 1. In the same VNET as the Application Gateway, create a Windows Server 2016 VM, add IIS to the VM and install the [.Net Core Windows Server Hosting Runtime](https://www.microsoft.com/net/download/core#/runtime)
-1. Clone this repository and publish the website to file.
+1. Clone this repository and publish the website to folder.
 1. Copy the published website to the VM, and replace the default IIS Application with this website.
 1. Add a listener to the Application Gateway so that you can navigate to the website externally.
 1. Launch the website.
 
-At this point you will see upload forms. The top one uses chunked uploading (via [resumable.js](http://www.resumablejs.com/)) and the bottom one is a standard upload via a post action. This is provided only for comparison, I have not been able to reproduce the issue with post action-based uploads.
+At this point you will see two upload forms. The top one uses chunked uploading (via [resumable.js](http://www.resumablejs.com/)) and the bottom one is a standard upload via a post action. This is provided only for comparison, I have not been able to reproduce the issue with post action-based uploads.
 
 Upload any file you want to see the error. Not all file have the issue but the chances increase as the size of the file increases. An example file that does reliably cause the issue is the [.Net Microservices Ebook](http://aka.ms/MicroservicesEbook).
 
